8697c7b47a7b8270151e80bf858b598d22bd6a1e
Philip.Mabon <philip.mabon@canada.ca>
2019-09-10 07:54:23 -0500

    Added and configured nginx in Dockerfile so we can run in a more production Galaxy env

--- inst/docker/shiny-server.sh
+++ inst/docker/shiny-server.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+
+sed -i "s| '\*'; # IE_CORS_ORIGIN| '${CORS_ORIGIN}';|g" /etc/nginx/sites-enabled/default;
+sed -i "s|PROXY_PREFIX|${PROXY_PREFIX}|g"               /etc/nginx/sites-enabled/default;
+
+
 # Make sure the directory for individual app logs exists
 mkdir -p /var/log/shiny-server
 chown shiny.shiny /var/log/shiny-server
@@ -12,5 +17,7 @@ echo "## End Galaxy Information ##" >> /etc/R/Renviron
 
 chmod +x /monitor_traffic.sh
 exec /monitor_traffic.sh &
+# And nginx in background mode.
+exec nginx &
 
 exec shiny-server >> /var/log/shiny-server.log 2>&1
