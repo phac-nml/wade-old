806047d12e37aa21d76c06e90dee0c7dfc5ef71c
Eric Enns <eric.enns@phac-aspc.gc.ca>
2019-10-03 10:31:21 -0500

    Added parsing of test and organism

--- R/wade_cmd.R
+++ R/wade_cmd.R
@@ -1,4 +1,4 @@
-sink(stdout(), type = "message")
+sink(stdout(), type = 'message')
 
 suppressPackageStartupMessages({
   library(optparse)
@@ -7,29 +7,60 @@ suppressPackageStartupMessages({
   library(here)
 })
 
-source(normalizePath("R/database_pipeline.R"))
+source(normalizePath('R/database_pipeline.R'))
+
+
+#' Check if value matches a pre-defined list of values
+check_choose_from <- function(choices) {
+  if (length(choices) == 0) {
+    stop(paste0('Choice is empty for "', opt_flag, '".'))
+  }
+  function(opt, opt_flag, opt_value, parser, choices. = choices) {
+    if (is.na(opt_value)) {
+      opt_value <- choices.[1]
+    } else if (length(opt_value) != 1) {
+      stop(
+        paste0('Expects a single value for "', opt_flag, '".'),
+        call. = FALSE
+      )
+    } else if (! opt_value %in% choices.) {
+      stop(
+        paste0(
+          'Supplied value "', opt_value, '" for "', opt_flag,
+          '" is not one of [', paste(choices., collapse = ', '), '].'
+        ),
+        call. = FALSE
+      )
+    }
+    opt_value
+  }
+}
 
 option_list = list(
-  make_option(c("--organism"),
-              type="character",
+  make_option(c('--organism'),
+              action='callback',
+              type='character',
               default=NULL,
-              help='organism',
-              metavar="character"),
-  make_option(c("-t", "--test"),
-              type="character",
+              metavar='character',
+              callback=check_choose_from(choices = c('GAS','GONO','PNEUMO')),
+              help='organism'),
+  make_option(c('-t', '--test'),
+              action='callback',
+              type='character',
               default=NULL,
-              help='test',
-              metavar="characater"),
-  make_option(c("-l", "--locus"),
-              type="character",
+              metavar='characater',
+              callback=check_choose_from(choices = c('TOXINS','MLST','VIRULENCE','EMM','AMR_DB','VFDB','NGSTAR','rRNA23S','NGMAST')),
+              help='test'),
+  make_option(c('-l', '--locus'),
+              type='character',
               default=NULL,
               help='locus',
-              metavar="character"),
-  make_option(c("-s","--samples"),
-              type="character",
+              metavar='character'),
+  make_option(c('-s','--samples'),
+              type='character',
               default=c(),
-              help="samples to analyse (comma separated list of files)",
-              metavar="character")
+              help='samples to analyse (comma separated list of files)',
+              metavar='character')
 )
 
 opt_parser = OptionParser(option_list=option_list)
@@ -37,10 +68,10 @@ opt = parse_args(opt_parser)
 
 if (is.null(opt$samples)) {
   print_help(opt_parser)
-  stop("Samples is a required argument", call.=FALSE)
+  stop('Samples is a required argument', call.=FALSE)
 }
 
-samples <- unlist(strsplit(opt$samples, ","))
+samples <- unlist(strsplit(opt$samples, ','))
 
 samples <- samples %>% map(~ normalizePath(.x))
 
@@ -48,7 +79,7 @@ samples <- samples %>% map_df(~ data.frame(fullpath = .x,
                              parent_dir = base::dirname(base::dirname(.x)),
                              subdir_id = base::basename(base::dirname(.x)),
                              filename = base::basename(.x),
-                             type = "file")
+                             type = 'file')
 )
 
 test <- 'AMR_DB'
