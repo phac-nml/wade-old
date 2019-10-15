bf54287af8f8167bac824ca9eb26acaa45952c3e
Adrian Zetner <adrian.zetner@canada.ca>
2019-10-15 14:18:10 -0500

    Added sourcetesting script temporarily: step througheach script and test function. Not to be kept in final

---
+++ R/sourcetesting.R
@@ -0,0 +1,10 @@
+# Temporary Script to Source Necessary WADE Files During Testing
+# 2019-10-15
+
+# Scripts added as initial testing done (run with wade_cmd.R )
+source(normalizePath('R/database_pipeline.R'))
+source(normalizePath('R/emm.R'))
+
+
+# This blast() function is only used in emm and generalmlst. DBpipeline uses a system command
+source(normalizePath('R/blast.R'))
